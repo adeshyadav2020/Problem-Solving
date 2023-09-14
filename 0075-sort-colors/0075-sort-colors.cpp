affadd54cddfabfe3639a880db8546275ebe6682
Adesh Pramod Yadav <95179737+adeshyadav2020@users.noreply.github.com>
2023-09-14 19:25:17 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (13.65%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
+        int zeroes, ones, twos;
+        zeroes = ones = twos=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0){
+                zeroes++;
+            }
+            else if(nums[i]==1){
+                ones++;
+            }
+            else {
+                twos++;
+            }
+        }
+        int i=0;
+        while(zeroes--){
+            nums[i]=0;
+            i++;
+        }
+        while(ones--){
+            nums[i]=1;
+            i++;
+        }
+        while(twos--){
+            nums[i]=2;
+            i++;
+        }
     }
 };
